5070579e5517d6052961e37c1e1885797604678f
Óscar García Amor <ogarcia@connectical.com>
2018-02-28 18:20:08 +0100

    Set project to PyPI
    Signed-off-by: Óscar García Amor <ogarcia@connectical.com>

--- lesma/__init__.py
+++ lesma/__init__.py
@@ -7,9 +7,10 @@
 # Distributed under terms of the GNU GPLv3 license.
 
 NAME = 'lesma'
-VERSION = '0.4.2'
-DESCRIPTION = 'Simple paste app, friendly with browser and command line, with files as storage backend.'
+VERSION = '0.5.0'
 AUTHOR_NAME = 'Óscar García Amor'
 AUTHOR_EMAIL = 'ogarcia@connectical.com'
-URL = 'https://github.com/connectical/pgpapi'
-LICENSE = 'GNU GPLv3'
+DESCRIPTION = 'Simple paste app, friendly with browser and command line, with files as storage backend.'
+KEYWORDS = 'lesma paste pastebin sprunge pastein hastebin friendly commandline'
+URL = 'https://lesma.eu'
+LICENSE = 'GPLv3'
--- setup.py
+++ setup.py
@@ -1,28 +1,36 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017-2018 Óscar García Amor <ogarcia@connectical.com>
+#
+# Distributed under terms of the GNU GPLv3 license.
 
 import lesma as project
 
-from setuptools import setup
-from setuptools import find_packages
-from pip.req import parse_requirements
+import os
 from pip.download import PipSession
+from pip.req import parse_requirements
+from setuptools import find_packages
+from setuptools import setup
 
-
-def get_file_content(fname):
-    try:
-        return open(fname).read()
-    except:
-        return None
-
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name=project.NAME,
-    version=project.VERSION,
-    description=project.DESCRIPTION,
-    long_description=get_file_content('README.md'),
-    author=project.AUTHOR_NAME,
-    author_email=project.AUTHOR_EMAIL,
-    url=project.URL,
+    name = project.NAME,
+    version = project.VERSION,
+    author = project.AUTHOR_NAME,
+    author_email = project.AUTHOR_EMAIL,
+    description = project.DESCRIPTION,
+    license = project.LICENSE,
+    keywords = project.KEYWORDS,
+    url = project.URL,
+    long_description=read('README.md'),
     packages=find_packages(),
     install_requires=[str(x.req) for x in
                       parse_requirements('requirements.txt',
@@ -36,7 +44,6 @@ setup(
             'app/templates/*'
             ]
     },
-    license=project.LICENSE,
     entry_points={
         'console_scripts': [
             'lesma = lesma.main:cli',
@@ -44,14 +51,18 @@ setup(
     },
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Programming Language :: Python :: 3.6',
         'Operating System :: OS Independent',
-        'Programming Language :: Python'
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
     ],
-
-    test_suite="tests.suite",
-    include_package_data=True,
+    project_urls={
+        'Bug Reports': 'https://github.com/ogarcia/lesma/issues',
+        'Source': 'https://github.com/ogarcia/lesma',
+    },
 )
